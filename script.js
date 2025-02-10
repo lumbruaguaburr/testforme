001136af216ef4966224c813b5ca3c45a59ee2f7
lumbruaguaburr <chinawatjunongweang@gmail.com>
2025-02-11 00:46:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.getElementById('sidebar');
     const menuToggle = document.querySelector('.menu-toggle');
     const closeBtn = document.querySelector('.close-btn');
-    const mainContent = document.getElementById('main-content');
 
     function toggleMenu() {
         sidebar.classList.toggle('active');
-        mainContent.classList.toggle('shifted');
     }
 
     menuToggle.addEventListener('click', toggleMenu);
